eadfb97344a95ae6ca60e9f0306f0a5d4a22e1d6
Oukaal17 <ccarcarius@gmail.com>
2022-02-09 12:12:05 +0100

    ver1

---
+++ js/data.js
@@ -0,0 +1,16 @@
+const food = [
+   bhat = {
+       nombre: 'Dhal Bhat',
+       precio: 10,
+       descipcion: 'Arros, lentejas'
+   },
+   momo = {
+       nombre: 'Mo:Mo',
+       precio: 8,
+       descipcion: 'Harina, Carne, mezclado con verduras'
+   }
+
+
+
+
+]
